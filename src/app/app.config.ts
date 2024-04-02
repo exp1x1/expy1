1d92cd4179ae27ce6aec1af07dc2a98737ee43c3
exp2 <gorvkrtianliya@gmail.com>
2024-04-02 14:17:45 +0530

    init angular/fire

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,38 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import {
+  getAnalytics,
+  provideAnalytics,
+  ScreenTrackingService,
+  UserTrackingService,
+} from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getPerformance, providePerformance } from '@angular/fire/performance';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import {
+  getRemoteConfig,
+  provideRemoteConfig,
+} from '@angular/fire/remote-config';
+import { environment } from 'src/environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes)],
+  providers: [
+    provideRouter(appRoutes),
+    importProvidersFrom(
+      provideFirebaseApp(() => initializeApp({ ...environment.fireConfig }))
+    ),
+    importProvidersFrom(provideAuth(() => getAuth())),
+    importProvidersFrom(provideAnalytics(() => getAnalytics())),
+    ScreenTrackingService,
+    UserTrackingService,
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+    importProvidersFrom(provideDatabase(() => getDatabase())),
+    importProvidersFrom(providePerformance(() => getPerformance())),
+    importProvidersFrom(provideStorage(() => getStorage())),
+    importProvidersFrom(provideRemoteConfig(() => getRemoteConfig())),
+  ],
 };
