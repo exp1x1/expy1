c8a1a3679ededb7c986d62aab426723c7a86dac8
exp2 <gorvkrtianliya@gmail.com>
2024-04-02 15:12:47 +0530

    route to dashboard when user

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/shared/data-access/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -8,4 +10,15 @@ import { CommonModule } from '@angular/common';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  constructor() {
+    effect(() => {
+      if (this.authService.user()) {
+        this.router.navigate(['dashboard']);
+      }
+    });
+  }
+}
