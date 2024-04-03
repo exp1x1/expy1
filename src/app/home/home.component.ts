eb32fd724ff4ba575aad6be1029612bd21c3449a
exp2 <gorvkrtianliya@gmail.com>
2024-04-03 13:25:13 +0530

    init home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component, effect, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { AuthService } from 'src/app/shared/data-access/auth.service';
+import { TextLogoComponent } from '../shared/ui/text-logo/text-logo.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TextLogoComponent, MatButtonModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
